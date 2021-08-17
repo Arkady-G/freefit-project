6c5e7b04efd5519acc119a10e97cebd3718d039c
Arkady G <gude@list.ru>
2021-08-17 19:08:44 +0300

    added fixed bags

--- js/script-for-workout.js
+++ js/script-for-workout.js
@@ -1,5 +1,12 @@
 "use strict"
 
+let dueDateWorkout = document.querySelectorAll('[data-due-date]');
+
+for (const todayWorkout of dueDateWorkout) {
+	todayWorkout.textContent = todayWorkout.dataset.dueDate;
+}
+
+
 let itemsWork = document.querySelectorAll('.current');
 let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
@@ -13,16 +20,12 @@ let dateCurrent = (`${fdDate} ${month[fdMonth]}`);
 
 for (const itemWork of itemsWork) {
 	let dateWrkout = itemWork.querySelector('.date');
-
 	let dateTable = dateWrkout.innerHTML;
 	let newDate = dateTable.replace(', 10:00', '');
-	console.log(newDate);
 
 	if (newDate === dateCurrent) {
-		console.log(true);
-		itemWork.style.background = "linear-gradient(180deg, #FBF138 0%, rgba(253, 249, 182, 0.45) 100%";
-		// itemWork.style.shadow = "0px 13px 30px rgba(186, 180, 69, 0.2)";
-		itemWork.style.opacity = "0.7";
+
+		itemWork.dataset.status = "today";
 	}
 }
 
